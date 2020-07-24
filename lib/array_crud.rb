96d3a5796ba8f4a5116feeecd05e38966697549b
Iain Robertson <iainrobertzon@gmail.com>
2020-07-24 19:27:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  gandalf_array = []
+  gandalf_array << "Gandalf"
 end
 
 def add_element_to_start_of_array(array, element)
