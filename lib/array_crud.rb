8c6c6c543ef9ae8893e1f0c242702bbed9d10648
Iain Robertson <iainrobertzon@gmail.com>
2020-07-24 19:34:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  hobit_array = ["Frodo", "Samwise", "Pippin", "Merry"]
-  hobit_array << "Bilbo"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
